2dbbfb50f47bf3b202c92070b4f370fc52ed382a
Ken Barrios <keniswaiting@gmail.com>
2017-03-05 11:36:06 -0600

    adds wage claim app project to data.js and images folder

--- app/data.js
+++ app/data.js
@@ -7,6 +7,19 @@
 		var list = [
 				{
 					id: '0001',
+					title: 'Wage Claim',
+					brief: 'Reimagining the Illinois Department of Labor Wage Claim website',
+					instructions1: null,
+					instructions2: null,
+					tech1: 'AngularJS + Typescript',
+					tech2: 'AngularJS Component Router',
+					tech3: 'Figuring out how to use Models to componentize data',
+					repo: 'https://github.com/kenbarrios/wage-claim-app/',
+					site: null,
+					img: '/kenbarrios.github.io/images/idol.png'
+				},
+				{
+					id: '0002',
 					title: 'LetterDrops',
 					brief: 'A game for kids to learn the ABCs and Keyboard Keys',
 					instructions1: 'There are two levels that involve falling “drops” with letters inside: the levels are called “Alphabetized” and “Randomized”. As the drops fall, the user types the corresponding letter into the keyboard to pop the drop.',
@@ -19,7 +32,7 @@
 					img: '/kenbarrios.github.io/images/letterDrops1.png'
 				},
 				{
-					id: '0002',
+					id: '0003',
 					title: 'Cause & Reflect',
 					brief: 'A front-end only meetup site for activists',
 					instructions1: null,
@@ -32,7 +45,7 @@
 					img: '/kenbarrios.github.io/images/cause-and-reflect.png'
 				},
 				{
-					id: '0003',
+					id: '0004',
 					title: 'Marvel Tap-iT',
 					brief: 'A comic book themed video game',
 					instructions1: 'Two players play on one desktop/laptop/tablet. After signing in, players choose one hero and one villian. The player on the left of the screen can attack the player on the right using the “A” button.',
@@ -43,19 +56,6 @@
 					repo: 'https://github.com/WDI-Woodstock-Schroeder/MarvelTapIt',
 					site: 'http://marveltapit.herokuapp.com/',
 					img: '/kenbarrios.github.io/images/marveltapit.png'
-				},
-				{
-					id: '0004',
-					title: 'Anvil',
-					brief: 'A front-end only news site',
-					instructions1: null,
-					instructions2: null,
-					tech1: 'first-ever project',
-					tech2: 'HTML and CSS to design the entire site, from scratch.',
-					tech3: 'Independently found necessary breakpoints for media queries',
-					repo: 'https://github.com/kenbarrios/kenbarrios.github.io/tree/master/GA-Circuits/unit9',
-					site: 'https://kenbarrios.github.io/GA-Circuits/unit9/index.html',
-					img: '/kenbarrios.github.io/images/anvil.png'
 				},
 		];
 		return list;
